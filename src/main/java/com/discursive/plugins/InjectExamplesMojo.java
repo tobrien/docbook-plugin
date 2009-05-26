a85efda86282a1882a50a9339c8c2d9c8a7b1aee
Tim O'Brien <tobrien@discursive.com>
2009-05-26 15:45:33 -0500

    More half-baked fugliness.   There is now an 'excerpt' attribute for programlisting that allows you to specify an identifier that is matched against a series of comments.   This allows us to inject a portion of a file as an example.

--- src/main/java/com/discursive/plugins/InjectExamplesMojo.java
+++ src/main/java/com/discursive/plugins/InjectExamplesMojo.java
@@ -3,6 +3,7 @@ package com.discursive.plugins;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -12,7 +13,6 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.TransformerFactoryConfigurationError;
@@ -24,6 +24,7 @@ import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.filefilter.WildcardFileFilter;
 import org.apache.commons.lang.StringUtils;
 import org.apache.maven.artifact.Artifact;
@@ -41,7 +42,6 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-
 
 /**
  * This Mojo simply verifies that each chapter has an appropriate identifer.
@@ -87,13 +87,13 @@ public class InjectExamplesMojo extends AbstractMojo {
 	 * @readonly
 	 */
 	private ArtifactFactory artifactFactory;
-	
-    /**
-     * To look up Archiver/UnArchiver implementations
-     *
-     * @component
-     */
-    protected ArchiverManager archiverManager;
+
+	/**
+	 * To look up Archiver/UnArchiver implementations
+	 * 
+	 * @component
+	 */
+	protected ArchiverManager archiverManager;
 
 	private List<String> validationFailures = new ArrayList<String>();
 
@@ -119,7 +119,11 @@ public class InjectExamplesMojo extends AbstractMojo {
 
 	}
 
-	private void processXml(File file) throws MojoFailureException, NoSuchArchiverException, MojoExecutionException, ParserConfigurationException, SAXException, IOException, XPathExpressionException, TransformerFactoryConfigurationError, TransformerException {
+	private void processXml(File file) throws MojoFailureException,
+			NoSuchArchiverException, MojoExecutionException,
+			ParserConfigurationException, SAXException, IOException,
+			XPathExpressionException, TransformerFactoryConfigurationError,
+			TransformerException {
 
 		String fileName = file.getName();
 		Document doc = parse(file.toURL(), true);
@@ -132,11 +136,11 @@ public class InjectExamplesMojo extends AbstractMojo {
 		NodeList nodes = (NodeList) result;
 		for (int i = 0; i < nodes.getLength(); i++) {
 			Element pl = (Element) nodes.item(i);
-			String artifact = pl.getAttributeNS( attributeNamespace, "artifact");
+			String artifact = pl.getAttributeNS(attributeNamespace, "artifact");
 			if (artifact != null && !artifact.equals("")) {
 				String groupId, artifactId, version, classifier, packaging;
 				String[] tokens = StringUtils.split(artifact, ":");
-				if (tokens.length != 5 )
+				if (tokens.length != 5)
 					throw new MojoFailureException(
 							"Invalid artifact, you must specify "
 									+ "groupId:artifactId:version:classifier:packaging"
@@ -147,67 +151,95 @@ public class InjectExamplesMojo extends AbstractMojo {
 				classifier = tokens[3];
 				packaging = tokens[4];
 
-				Artifact toDownload = artifactFactory.createArtifactWithClassifier(groupId, artifactId, version, packaging, classifier);
-				System.out.println( toDownload.toString() );
-				
-				String path = localRepository.pathOf( toDownload );
-				System.out.println( path );
-				
-				File artifactFile = new File( localRepository.getBasedir(), path );
-				
-				String filename = pl.getAttributeNS( attributeNamespace, "file" );
-				String xpath1 = pl.getAttributeNS( attributeNamespace, "xpath" );
-				
+				Artifact toDownload = artifactFactory
+						.createArtifactWithClassifier(groupId, artifactId,
+								version, packaging, classifier);
+				System.out.println(toDownload.toString());
+
+				String path = localRepository.pathOf(toDownload);
+				System.out.println(path);
+
+				File artifactFile = new File(localRepository.getBasedir(), path);
+
+				String filename = pl.getAttributeNS(attributeNamespace, "file");
+
 				UnArchiver unArchiver;
 
-	            unArchiver = archiverManager.getUnArchiver( artifactFile );
+				unArchiver = archiverManager.getUnArchiver(artifactFile);
 
-	            unArchiver.setSourceFile( artifactFile );
+				unArchiver.setSourceFile(artifactFile);
 
-	            String property = "java.io.tmpdir";
-	            // Get the temporary directory and print it.
-	            String tempDirPath = System.getProperty(property);
-	            
-	            File tempDir = new File( tempDirPath );
-	            try {
-					unArchiver.extract( filename, tempDir );
+				String property = "java.io.tmpdir";
+				// Get the temporary directory and print it.
+				String tempDirPath = System.getProperty(property);
+
+				File tempDir = new File(tempDirPath);
+				try {
+					unArchiver.extract(filename, tempDir);
 				} catch (ArchiverException e) {
-					throw new MojoExecutionException( "Error finding archiver for file" );
+					throw new MojoExecutionException(
+							"Error finding archiver for file");
 				}
-			
-				File workingFile = new File( tempDir, filename );
-				System.out.println( "Working File: " + workingFile.toString() );
-				
-				Document workingDoc = parse( workingFile.toURL(), false );
-				XPath xpath2 = factory.newXPath();
-				XPathExpression expr2 = xpath2.compile(xpath1);
-				System.out.println( xpath1 );
-				Object result2 = expr2.evaluate(workingDoc, XPathConstants.NODESET);
-				NodeList result2List = (NodeList) result2;
-				System.out.println(  );
-			
-				ByteArrayOutputStream baos = new ByteArrayOutputStream();
-				StreamResult result1 = new StreamResult( baos );
-				DOMSource domSource = new DOMSource( result2List.item( 0 ) );
-				Transformer transformer = TransformerFactory.newInstance().newTransformer();
-				transformer.setOutputProperty("omit-xml-declaration","yes");			
-				transformer.transform( domSource, result1);
-				String content = baos.toString("UTF-8");
-				System.out.println( content );
-				pl.setTextContent( content );
-				
+
+				File workingFile = new File(tempDir, filename);
+				System.out.println("Working File: " + workingFile.toString());
+
+				String xpath1 = pl.getAttributeNS(attributeNamespace, "xpath");
+				String excerpt = pl.getAttributeNS( attributeNamespace, "excerpt");
+
+				if (xpath1 != null && !StringUtils.isEmpty(xpath1)) {
+					Document workingDoc = parse(workingFile.toURL(), false);
+					XPath xpath2 = factory.newXPath();
+					XPathExpression expr2 = xpath2.compile(xpath1);
+					System.out.println(xpath1);
+					Object result2 = expr2.evaluate(workingDoc,
+							XPathConstants.NODESET);
+					NodeList result2List = (NodeList) result2;
+					System.out.println();
+
+					ByteArrayOutputStream baos = new ByteArrayOutputStream();
+					StreamResult result1 = new StreamResult(baos);
+					DOMSource domSource = new DOMSource(result2List.item(0));
+					Transformer transformer = TransformerFactory.newInstance()
+							.newTransformer();
+					transformer
+							.setOutputProperty("omit-xml-declaration", "yes");
+					transformer.transform(domSource, result1);
+					String content = baos.toString("UTF-8");
+					System.out.println(content);
+					pl.setTextContent(content);
+				} else if( excerpt != null && !StringUtils.isEmpty( excerpt )){
+					
+					String fileContent = IOUtils.toString( new FileInputStream( workingFile ) );
+					
+					String startString = "// START " + excerpt;
+					String endString = "// END " + excerpt;
+					
+					String excerptContent = fileContent.substring( fileContent.indexOf( startString ) + startString.length(),
+																   fileContent.indexOf( endString ) );
+					excerptContent = StringUtils.replace( excerptContent, "\r\n", "\n");
+					excerptContent = StringUtils.replace( excerptContent, "\t", "  " );
+					pl.setTextContent( excerptContent );
+					
+				}
+
 			}
-			
+
 		}
+
+		StreamResult result1 = new StreamResult(file.toURI().getPath());
+		DOMSource domSource = new DOMSource(doc);
+		Transformer transformer = TransformerFactory.newInstance()
+				.newTransformer();
 		
-		StreamResult result1 = new StreamResult( file.toURI().getPath() );
-		DOMSource domSource = new DOMSource( doc );
-		Transformer transformer = TransformerFactory.newInstance().newTransformer();
-		transformer.transform( domSource, result1);
+		transformer.setOutputProperty( "doctype-public", "-//OASIS//DTD DocBook XML V4.5//EN");
+		transformer.setOutputProperty( "doctype-system", "http://www.oasis-open.org/docbook/xml/4.5/docbookx.dtd");
+		transformer.transform(domSource, result1);
 
 	}
 
-	public Document parse(URL url, boolean namespaceAware) throws ParserConfigurationException, SAXException, IOException {
+	public Document parse(URL url, boolean namespaceAware)
+			throws ParserConfigurationException, SAXException, IOException {
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		factory.setNamespaceAware(namespaceAware); // never forget this!
 		DocumentBuilder builder = factory.newDocumentBuilder();
